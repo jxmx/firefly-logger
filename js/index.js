d83a63fefcd5f77d297b5190f7ba7fc0e8c4f1d2
Jason D. McCormick <jason@mfamily.org>
2021-06-16 18:14:30 -0400

    abstract statusmsg box logic

--- js/index.js
+++ js/index.js
@@ -272,58 +272,12 @@ function logReset() {
     document.getElementById("call").focus();
 };
 
-//
-// Status Area Functions
-// 
-function goodStatusMsg(msg) {
-	var statusbox = document.getElementById("statusarea");
-	clearStatusMsg();
-	statusbox.classList.remove("invisible");
-	statusbox.classList.add("alert-success");
-	statusbox.innerHTML += "<svg class=\"bi flex-shrink-0 me-2\" width=\"24\" height=\"24\" role=\"img\" aria-label=\"Success:\"><use xlink:href=\"#check-circle-fill\"/></svg>";
-	statusbox.innerHTML += "<div class=\"d-inline-flex\">" + msg + "</div>";
-	statusbox.innerHTML += "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>";
-};
-
-function decayingGoodStatusMsg(msg, secs) {
-	setTimeout(clearStatusMsg, secs * 1000);
-	goodStatusMsg(msg);
-}
-
-function alertStatusMsg(msg) {
-	var statusbox = document.getElementById("statusarea");
-	clearStatusMsg();
-	statusbox.classList.remove("invisible");
-	statusbox.classList.add("alert-danger");
-	statusbox.innerHTML += "<svg class=\"bi flex-shrink-0 me-2\" width=\"24\" height=\"24\" role=\"img\" aria-label=\"Success:\"><use xlink:href=\"#exclamation-triangle-fill\"/></svg>";
-	statusbox.innerHTML += "<div class=\"d-inline-flex\">" + msg + "</div>";
-	statusbox.innerHTML += "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>";
-};
-
-function decayingAlertStatusMsg(msg, secs) {
-    setTimeout(clearStatusMsg, secs * 1000);
-    alertStatusMsg(msg);
-}
-
-
-
-function clearStatusMsg(){
-	var statusbox = document.getElementById("statusarea");
-	statusbox.classList.remove("alert-success", "alert-danger");
-    statusbox.classList.add("invisible");
-	statusbox.innerHTML = "";
-}
-
-function testMsg(){
-	alertStatusMsg("This is a test!");
-}
-
 // 
 // Manage QSOs
 //
 
 function editQSO(qkey){
-	alert("Function not implemented");
+	window.open("editqso.php?qkey=" + qkey, "_blank");
 }
 
 function delQSO(qkey, qcall){
