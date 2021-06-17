4ac94bda95cb3adfe052684cc6eee4a3f50682b4
Jason D. McCormick <jason@mfamily.org>
2021-06-17 12:28:36 -0400

    implement board

---
+++ api/board.php
@@ -0,0 +1,55 @@
+<?php
+header('Content-Type: text/plain');
+
+function getGetVar($id) {
+	return filter_var(trim($_GET[$id]), FILTER_SANITIZE_STRING);
+}
+
+$q = getGetVar("q");
+
+include("db.php");
+
+switch ($q){
+	case "total":
+		$qry = "SELECT COUNT(qkey) FROM qso";
+		break;
+	case "phone":
+		$qry = "SELECT COUNT(qkey) FROM qso WHERE mode='Phone'";
+		break;
+
+	case "cw":
+		$qry = "SELECT COUNT(qkey) FROM qso WHERE mode='CW'";
+		break;
+
+	case "digital":
+		$qry = "SELECT COUNT(qkey) FROM qso WHERE mode='Digital'";
+		break;
+
+	case "distinct":
+		$qry = "SELECT COUNT(DISTINCT(callsign)) FROM qso";
+		break;
+
+	case "sections":
+		$qry = "SELECT COUNT(DISTINCT(section)) FROM qso";
+		break;
+
+	// print totals by default
+	default:
+		$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ||
+			 $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+		$myurl = sprintf("%s%s%s?q=", $protocol, $_SERVER['SERVER_NAME'], $_SERVER['SCRIPT_NAME']);
+		$qsop  = file_get_contents($myurl . "phone");
+		$qsoc  = file_get_contents($myurl . "cw");
+		$qsod  = file_get_contents($myurl . "digital");
+		printf("%d", $qsop + ( $qsoc * 2 ) + ( $qsod * 2 ));
+		exit;
+
+}
+if($res = $db->query($qry)){
+	$row = $res->fetch_row();
+	print $row[0];
+} else {
+	print "DB ERROR";
+}
+$db->close();
+?>
---
+++ js/board.js
@@ -0,0 +1,35 @@
+//
+// For board.html
+//
+
+// fields to populate
+const fields = [ "total" , "phone" , "cw" , "digital"  , "sections" , "distinct", "score" ];
+
+// load / timer
+window.addEventListener("load", function(){
+	doFieldUpdates();
+	setInterval(doFieldUpdates, 60000);
+});
+
+
+// function to do the iterations
+function doFieldUpdates(){
+	fields.forEach(getFieldData);
+}
+
+// Ajaxification
+function getFieldData(field){
+	$.ajax({
+		type:   "get",
+		url:    "api/board.php?q=" + field,
+		success: function(data) { updateFieldData(field, data); },
+		failure: function(msg) { console.log(msg); }
+    });
+}
+
+// abstrat this for no real reason 
+function updateFieldData(field, value) {
+	document.getElementById(field).innerHTML = value;
+}
+
+
