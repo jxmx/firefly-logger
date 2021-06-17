d6b9f6c157d0392ee095de8a3c88030000df3599
Jason D. McCormick <jason@mfamily.org>
2021-06-17 11:42:13 -0400

    add option to hide the edit buttons

--- api/displaylog.php
+++ api/displaylog.php
@@ -1,5 +1,12 @@
 <?php
 
+function getGetVar($id) {
+    return filter_var(trim($_GET[$id]), FILTER_SANITIZE_STRING);
+}
+
+$hidebuttons = getGetVar("hidebuttons");
+
+
 include("db.php");
 
 $insqry = "select * from ( select * from qso order by date desc limit 10 ) sub order by date desc;";
@@ -14,12 +21,18 @@ if($res = $db->query($insqry)){
 		printf("<td class=\"align-middle\">%s</td>\n", $row["mode"]);
 		printf("<td class=\"align-middle\">%s</td>\n", $row["operator"]);
 		print "<td class=\"align-middle\">\n";
-		print "<div cass=\"btn-group\" role=\"group\">\n";
-		printf("<button type=\"button\" class=\"btn btn-primary btn-warning btn-sm\" onclick=\"editQSO('%d')\">Edit</button>\n", 
-			$row["qkey"]);
-		printf("<button type=\"button\" class=\"btn btn-primary btn-danger btn-sm\" onclick=\"delQSO('%d','%s')\">Del</button>\n", 
+		
+		if( $hidebuttons == "y" ){
+			print "&nbsp;";
+
+		} else {
+			print "<div cass=\"btn-group\" role=\"group\">\n";
+			printf("<button type=\"button\" class=\"btn btn-primary btn-warning btn-sm\" onclick=\"editQSO('%d')\">Edit</button>\n", 
+				$row["qkey"]);
+			printf("<button type=\"button\" class=\"btn btn-primary btn-danger btn-sm\" onclick=\"delQSO('%d','%s')\">Del</button>\n", 
 			$row["qkey"], $row["callsign"]);
-		print "</div>\n";
+			print "</div>\n";
+		}
 		print "</td>\n";
 		print "</tr>\n";
 
