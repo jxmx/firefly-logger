dcd21c3205d2355227dba6d04566b5a36922c46e
Jason McCormick <jason@mfamily.org>
2022-04-26 17:03:41 -0400

    implement JSON/remote config 1

--- js/index.js
+++ js/index.js
@@ -1,12 +1,95 @@
 //
-// Things that might require configuration
+// Configuration Helpers
 //
 
-// Add/edit/delete the ARRL/RAC sections from this array as things change
-// Keep everything ordered by size (i.e. 2 char sections first, then 3 char sections) and then alphabetical
-const sections = [ "AB","AK","AL","AR","AZ","BC","CO","CT","DE","DX","EB","GA","IA","ID","IL","IN","KS","KY","LA","MB","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NL","NM","NT","NV","OH","OK","OR","PE","PR","QC","RI","SB","SC","SD","SF","SK","SV","TN","UT","VA","VI","VT","WI","WV","WY","EMA","ENY","EPA","EWA","GTA","LAX","MAR","MDC","NFL","NLI","NNJ","NNY","NTX","ONE","ONN","ONS","ORG","PAC","SCV","SDG","SFL","SJV","SNJ","STX","WCF","WMA","WNY","WPA","WTX","WWA" ]
 
+// top level configuration variables
+var APIPrefix = "/api";			// prefix for where the API is located on the webserver
+
+// master configuration for the application itself
+let config = {					
+	"general": [ 
+		{"stationCall" : "" },
+		{ "fdType": ""},
+		{ "multiOp": ""}
+	],
+	"bands": null,
+	"modes": null,
+	"sections": null,
+};
+
+// statuses and counters
 var qsinceload = 0;
+
+// load configuration from JSON files
+function getConfig(configType){
+	var xmlhttp = new XMLHttpRequest();
+	var url = APIPrefix + "/config_" + configType + ".json";
+	xmlhttp.onreadystatechange = function () {
+		if( this.readyState == 4 && this.status == 200 ){
+			var a = JSON.parse(this.responseText);
+			switch(configType){
+				case "general":
+					console.log("get config general");
+					config.general = a;
+					console.log("set config general");
+					setGeneralConfig();
+					break;		
+				case "bands":
+					console.log("get config bands");
+					config.bands = a;
+					refreshBandList(config.bands.band);
+					setStationDataFromCookie();
+					break;
+				case "modes":
+					console.log("get config modes");
+					config.modes = a;
+					refreshModeList(config.modes.modes);
+					setStationDataFromCookie();
+					break;
+				case "sections":
+					console.log("get config sections");
+					config.sections = a;
+					break;
+			}
+		} else if( this.readyState == 4 && this.status != 200 ){
+			console.log("Failed to retreive configuration: " + url);
+			console.log("Ready State: " + this.readyState);
+			console.log("HTTP Status: " + this.status);
+			alert("Error retriving configuration file - check console");
+		}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+//
+// apply general configuration - used inside getConfig() only because it's async
+//
+function setGeneralConfig(){
+	// set the station callsign
+	document.getElementById("callsign").value = config.general.stationCall;
+
+	// show/hit the Operator box
+	if( config.general.multiOp == false){
+		document.getElementById("operator").readOnly = true;
+		document.getElementById("operator").value = config.general.stationCall;
+		submitOkOperCall = true;
+	}
+}
+
+//
+// On the load
+//
+window.addEventListener("load", function(){
+	getConfig("general");
+	getConfig("bands");
+	getConfig("modes");
+	getConfig("sections");
+	setGeneralConfig();
+	updateLogTime();
+	setInterval(updateLogTime,1000);
+	});
 
 //
 // QKey
@@ -18,46 +101,9 @@ function qkeyCalculate(qsocall, opband, opmode){
 	return(a+b+c);
 }
 
-//
-// Type-ahead searching for section box
-//
-
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
-
-    // an array that will be populated with substring matches
-    matches = [];
-
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
-
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
-
-    cb(matches);
-  };
-};
-
-$('#arrl-sections .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'sections',
-  source: substringMatcher(sections)
-});
-
 // 
 // Logging clock for QSOs
 //
-
 function currentTimestamp() {
 	function pad(n) { return n<10 ? '0'+n : n }
 	var d = new Date();
@@ -73,10 +119,7 @@ function updateLogTime() {
 	document.getElementById("logclock").value = currentTimestamp();
 }
 
-window.addEventListener("load", function(){
-	updateLogTime();
-	setInterval(updateLogTime,1000);
-	});
+
 
 //
 // Field Validators
@@ -87,7 +130,9 @@ var submitOkClass = false;
 var submitOkSection = false;
 var submitOkDupe = false;
 
-// QSO Call
+//
+// QSO Call Validation - is callsign well-formed
+//
 $('#call').on('input', function() {
 	var input=$(this);
 	var re = /^[a-zA-Z0-9\/]{2,9}[a-zA-Z]$/;
@@ -101,9 +146,10 @@ $('#call').on('input', function() {
 	}
 });
 
-
+//
 // Duplicate checking logic
 // Note: this has to be async because of XHR and then call a different function to lock stuff out
+//
 function isDupeQSO() {
     var opband = document.getElementById("band").value;
     document.getElementById("opband").value = opband;
@@ -144,12 +190,14 @@ function handleIsDupeQSO(resp) {
 	}
 }
 
-
+//
+// Validate the Operating Class for wellformedness
+//
 $('#opclass').on('input', function() {
     var input=$(this);
 	var re;
 
-	if( document.getElementById("fdtype").value == "WFD"){
+	if( config.general.fdType == "WFD"){
 		re = /^[0-9]{1,2}[hioHIO]$/;
 	} else {
 		re = /^[0-9]{1,2}[a-fA-F]$/;
@@ -165,10 +213,49 @@ $('#opclass').on('input', function() {
     }
 });
 
+//
+// Type-ahead searching for section box
+//
+var substringMatcher = function(strs) {
+	return function findMatches(q, cb) {
+	  var matches, substringRegex;
+  
+	  // an array that will be populated with substring matches
+	  matches = [];
+  
+	  // regex used to determine if a string contains the substring `q`
+	  substrRegex = new RegExp(q, 'i');
+  
+	  // iterate through the pool of strings and for any string that
+	  // contains the substring `q`, add it to the `matches` array
+	  $.each(strs, function(i, str) {
+		if (substrRegex.test(str)) {
+		  matches.push(str);
+		}
+	  });
+  
+	  cb(matches);
+	};
+  };
+
+//
+// Validate the Section for correctness
+//
+$('#arrl-sections .typeahead').typeahead({
+	hint: true,
+	highlight: true,
+	minLength: 1
+  },
+  {
+	name: 'sections',
+	source: substringMatcher(config.sections)
+  });
+
 $('#section').on('input', function() {
     var input=$(this);
 	document.getElementById("section").value = input.val().toUpperCase();
-    if(sections.includes(input.val().toUpperCase())){
+	config.sections.includes
+    if(config.sections.sections.includes(input.val().toUpperCase())){
 		submitOkSection = true;		
         input.removeClass("is-invalid").addClass("is-valid");
     } else {
@@ -177,6 +264,11 @@ $('#section').on('input', function() {
     }
 });
 
+//
+// Store contact in log functions
+//
+
+// Prevent logging button from working if QSO isn't complete/valid
 $('#log').on('input', function() {
 	if(checkSubmitOkStatus() && checkStationSetOkStatus()){
 		toggleLogButton(true);
@@ -196,12 +288,7 @@ function toggleLogButton(bbool) {
 	}
 };
 
-
-
-//
-// Submit LOG
-//
-
+// intercept enter and escape for log entry/clearing
 $(document).on('keyup', function(k) {
 	if(k.key == "Enter") logSubmit();
 	if(k.key == "Escape") logReset();
@@ -304,7 +391,6 @@ function logReset() {
 // 
 // Manage QSOs
 //
-
 function editQSO(qkey){
 	window.open("editqso.php?qkey=" + qkey, "_blank");
 }
@@ -333,3 +419,146 @@ function handleDelQSO(output, qkey, qcall){
 	}		
 }
 
+//
+// All of the Station Setup information
+//
+
+//
+// Field validators
+//
+
+var submitOkOperCall = false;
+var submitOkBand = false;
+var submitOkMode = false;
+
+// Is the station info completed
+function checkStationSetOkStatus() {
+	if( submitOkOperCall && submitOkBand && submitOkMode ){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// Operator Callsign
+$('#operator').on('input', function() {
+    var input=$(this);
+    var re = /^[a-zA-Z0-9\/]{2,9}[a-zA-Z]$/;
+    var is_valid = re.test(input.val());
+    if(is_valid){
+        submitOkOperCall = true;
+        input.removeClass("is-invalid");
+    } else {
+        submitOkOperCall= false;
+        input.addClass("is-invalid");
+    }
+});
+
+//
+// Band Configuration Dropdown
+//
+function refreshBandList(blist){
+    var r = "<option value=\"X\">Select Band...</option>";
+    blist.forEach(
+        function(v) {
+            r += `<option value="${v}">${v}</option>`;
+        }
+    )
+
+    document.getElementById("band").innerHTML = r;
+}
+
+$('#band').on('input', function() {
+    var input=$(this);
+    if(input.val() !== "X" ){
+        submitOkBand = true;
+        input.removeClass("is-invalid");
+    } else {
+        submitOkBand= false;
+        input.addClass("is-invalid");
+    }
+});
+
+//
+// Mode Configuration Dropdown
+//
+function refreshModeList(mlist){
+    var r = "<option value=\"X\">Select Mode...</option>";
+    for( let m of mlist){
+        r += `<option value="${m['key']}">${m['value']}</option>`;
+    }
+    document.getElementById("mode").innerHTML = r;
+}
+
+$('#mode').on('input', function() {
+    var input=$(this);
+    if(input.val() !== "X" ){
+        submitOkMode = true;
+        input.removeClass("is-invalid");
+    } else {
+        submitOkMode= false;
+        input.addClass("is-invalid");
+    }
+});
+
+
+// Read the data from the cookie and set it on page load
+
+
+// Generic set method
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000 ));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + "; SameSite=Lax;";
+};
+
+// Generic get method
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+// Set Station from Form
+function saveStationData() {
+	if( checkStationSetOkStatus() ){
+		setCookie("operator", document.getElementById("operator").value, 30);
+		setCookie("band", document.getElementById("band").value, 30);
+		setCookie("mode", document.getElementById("mode").value, 30);
+	} else {
+		alertStatusMsg("Station information not complete");
+	}
+};
+
+
+// Set Station from Cookie on Load
+function setStationDataFromCookie() {
+
+	var re = /^[a-zA-Z0-9\/]{2,9}[a-zA-Z]$/;
+
+	if( ( operator = getCookie("operator") ) && re.test(getCookie("operator")) ) {
+		document.getElementById("operator").value = operator;
+		submitOkOperCall = true;
+	}
+
+	if( ( band = getCookie("band") ) && ( getCookie("band") !== "X" ) ) {
+		document.getElementById("band").value = band;
+		submitOkBand = true;
+	}
+
+	if( ( mode = getCookie("mode") ) && ( getCookie("mode") !== "X" ) ) {
+		document.getElementById("mode").value = mode;
+		submitOkMode = true;
+	}
+}
