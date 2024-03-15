0412e44a69dc273d13acfb42caefcb966621bb60
Jason D. McCormick <jason@mfamily.org>
2024-03-15 12:25:48 -0400

    check for all vars before attempting insert

--- api/storeqso.php
+++ api/storeqso.php
@@ -12,6 +12,11 @@ $operator = getPostVar("opoperator");
 $band = getPostVar("opband");
 $mode = getPostVar("opmode");
 
+if( ! ( $qkey || $logclock || $call || $opclass || $section || $callsign || $operator || $band || $mode ) ){
+	echo "ERROR missing param";
+	exit;
+}
+
 include("db.php");
 
 $insqry = sprintf("INSERT INTO qso VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s');",
