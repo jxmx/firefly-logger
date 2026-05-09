60c875cdfd3dc78f30eaeadbbe2bae97ef80f976
Jason McCormick <jason@mfamily.org>
2026-05-09 15:34:17 -0400

    add map

---
+++ web/map.php
@@ -0,0 +1,22 @@
+<?php
+
+// This is the page title in <head>>. It's followed by "| Firefly"
+$ff_page_title = "Map";
+
+// This is centered content in the header
+$ff_header_content = <<<EOT
+	<h2>Map</h2>
+	EOT;
+
+$ff_additional_css = '<style>
+    .ff-page-wrapper { height: 100%; }
+</style>';
+
+$ff_additional_scripts = <<<EOT
+EOT;
+include("header.php");
+?>
+<main>
+
+</main>
+<?php include("footer.php"); ?>
